da24aa49997ad7e3e564ea6055f7d4dfa4ddd0dd
Hairuo Li <70992334+Li-610@users.noreply.github.com>
2024-08-24 20:45:09 -0400

    Update GuestHomePage.js

--- src/components/GuestHomePage.js
+++ src/components/GuestHomePage.js
@@ -1,30 +1,34 @@
 import React from "react";
-import {
-  Image,
-  message,
-  Tabs,
-  List,
-  Typography,
-  Form,
-  InputNumber,
-  DatePicker,
-  Button,
-  Card,
-  Carousel,
-  Modal,
-} from "antd";
-import {
-  bookStay,
-  cancelReservation,
-  getReservations,
-  searchStays,
-} from "../utils";
-import { LeftCircleFilled, RightCircleFilled } from "@ant-design/icons";
+import { Button, Layout, Dropdown } from "antd";
 
-class GuestHomePage extends React.Component {
-  render() {
-    return <>GuestHomePage</>;
-  }
+import { UserOutlined } from "@ant-design/icons";
+
+const { Header, Content } = Layout;
+
+function GuestHomePage() {
+  return (
+    <Layout>
+      <Header
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          backgroundColor: "transparent",
+          borderBottom: "2px solid #ccc",
+        }}
+      >
+        <div>User Name</div>
+        {
+          <div>
+            <Button>Create Plan</Button>
+          </div>
+        }
+      </Header>
+      <Content>
+        <p>Toronto, Ontario, Canada</p>
+        <p>2021/04/08 10:21:13</p>
+      </Content>
+    </Layout>
+  );
 }
 
 export default GuestHomePage;
